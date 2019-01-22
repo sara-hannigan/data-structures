11d85d692b3748d9cda82c7bfd5b9e7d1f031f72
sara-hannigan <sara.hannigan@hotmail.com>
2019-01-21 18:54:54 -0800

    failing on test 8

--- is_bst/is_bst.cpp
+++ is_bst/is_bst.cpp
@@ -15,55 +15,91 @@ struct Node {
 	Node() : key(0), left(-1), right(-1) {}
 	Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
+//
+//int traversalHelper(const vector<Node>& tree, int location) {
+//	if (location == -1) {
+//		//cout << "Do I make it here before I break";
+//		tree[location].key;
+//	}
+//	return inOrderTraversal(tree, tree[location].left) && inOrderTraversal(results, tree, tree[location].right);
+//
+//
+//
+//}
 
-bool inOrderTraversal(const vector<Node>& tree, int location, int preVal, int direction) {
-	// Location = index
-	//Direction 0 is left
-	//Direction 1 is right
-	if (location == -1) {
-		//cout << "Do I make it here before I break";
-		return true;
-	}
-	cout << "Current value of node is " << tree[location].key << endl;
-	if ((preVal != -1) && (preVal != 0)) {
-		if (direction == 0) {
-			if (tree[location].key >= preVal) {
-				cout << "Left and key is greater than the previous value" << tree[location].key << " " << preVal << endl;
-				return false;
-			}
-			if (tree[location].key > tree[0].key) {
-				cout << "Left and key is greater than the root" << tree[location].key << " " << tree[0].key << endl;
+class traversal {
+private:
+	vector<int> answerResults;
+
+public:
+	bool inOrderTraversal(const vector<Node>& tree, int location) {
+		//cout << "Key is " << tree[location].key << endl;
+		if (location == -1) {
+			return true;
+		}
+		int leftLocation = tree[location].left;
+		if (leftLocation != -1) {
+			if (tree[leftLocation].key >= tree[location].key) {
+				//cout << "left key is " << tree[leftLocation].key << endl;
 				return false;
 			}
 		}
-		if (direction == 1) {
-			if (tree[location].key < preVal) {
-				cout << "Right and key is less than the previous val " << tree[location].key << " " << preVal << endl;
-				return false;
-			}
-			if (tree[location].key < tree[0].key) {
-				cout << "Right and key is greater than the root" << tree[location].key << " " << tree[0].key << endl;
-				return false;
-			}
+		//if (answerResults.size() > 0) {
+		//	cout << "Back of answerResults is " << answerResults.back() << endl;
+		//	if (tree[location].key > answerResults.back()) {
+		//		cout << "Key in breaking point is " << tree[location].key << endl;
+		//		return false;
+		//	}
 
+		//}
+		answerResults.push_back(tree[location].key);
+		inOrderTraversal(tree, tree[location].left);
+		//cout << "Right location " << tree[location].right;
+		int rightLocation = tree[location].right;
+		if (tree[location].key < answerResults.back()) {
+			//cout << "Back of answerResults is " << answerResults.back() << endl;
+			//cout << "Key in breaking point is " << tree[location].key << endl;
+			//cout << "Do I even make it here 3" << endl;
+			return false;
 		}
+		else {
+			inOrderTraversal(tree, tree[location].right);
+		}
+
+		//if (answerResults.back()) {
+		//	if (tree[rightLocation].key < answerResults.back()) {
+		//		cout << "Do I even make it here 4" << endl;
+		//		return false;
+		//	}
+		//}
 	}
-	return inOrderTraversal(tree, tree[location].left, tree[location].key, 0) and inOrderTraversal(tree, tree[location].right, tree[location].key, 1);
-}
+
+};
 
 bool IsBinarySearchTree(const vector<Node>& tree) {
-	//cout << "Do I make it here \n";
-	bool results = inOrderTraversal(tree, 0, 0, 0);
+	traversal inOrder;
+	bool results;
+	results = inOrder.inOrderTraversal(tree, 0);
 	return results;
-	// Implement correct algorithm here
-	  //Probably recursion, 
-	  //set to return false any time the nodes are larger on the left or smaller on the right
-	  //Check if left val smaller than current val, then recurse down
-	  //Do opposite for right
-	  //save a max and a min value?....is that necessary?
-	  //I feel like left/right should just always be smaller or larger
-	  //keep in mind equal conditions, node should be on the right
-	  //OR consider an in-order traversal?
+	//for (int i = 0; i < results.size(); ++i) {
+	//	//cout << results[i] << endl;
+	//	if (i + 1 < results.size()) {
+	//		if (results[i] > results[i + 1]) {
+	//			//cout << "smaller result " << results[i] << endl;
+	//			//cout << "larger result " << results[i + 1] << endl;
+	//			return false;
+	//		}
+	//	}
+  // Implement correct algorithm here
+	//Probably recursion, 
+	//set to return false any time the nodes are larger on the left or smaller on the right
+	//Check if left val smaller than current val, then recurse down
+	//Do opposite for right
+	//save a max and a min value?....is that necessary?
+	//I feel like left/right should just always be smaller or larger
+	//keep in mind equal conditions, node should be on the right
+	//OR consider an in-order traversal?
+	return true;
 }
 
 int main() {
